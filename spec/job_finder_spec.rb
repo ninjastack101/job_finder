81026d3331e610d2233f04f713fa9ca4297e904e
ninjastack101 <ninjastack101@gmail.com>
2018-12-11 14:38:30 +0530

    initial commit

---
+++ spec/job_finder_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe JobFinder do
+  it "has a version number" do
+    expect(JobFinder::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
